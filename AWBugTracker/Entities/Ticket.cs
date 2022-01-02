f47a1793f8eff62e8f39b4b3eae0992143d25a5c
Ashley Laswell <ashleylaswell@rocketmail.com>
2022-01-02 12:20:27 -0500

    add ticket entity and link it to project entity

--- AWBugTracker/Entities/Ticket.cs
+++ AWBugTracker/Entities/Ticket.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AWBugTracker.Entities
 {
-    public class Project
+    public class Ticket
     {
+        public Ticket()
+        {
+            DateTimeTicketCreated = DateTime.Now;
+        }
         public int Id { get; set; }
         [Required]
         [StringLength(200, MinimumLength = 2)]
         public string Title { get; set; }
         public string Description { get; set; }
-        public string ThumbnailImagePath { get; set; }
-        [ForeignKey("ProjectId")]
-        public ICollection<Ticket> Tickets { get; set; }
+        public int ProjectId { get; set; }
+        public int BugTypeId { get; set; }
+        public DateTime DateTimeTicketCreated { get; private set; }
     }
 }
