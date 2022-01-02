6161fca7cf109c745451764bc399a3ae5079b116
Ashley Laswell <ashleylaswell@rocketmail.com>
2022-01-02 16:43:48 -0500

    add admin area

--- AWBugTracker/Startup.cs
+++ AWBugTracker/Startup.cs
@@ -58,9 +58,13 @@ namespace AWBugTracker
 
             app.UseAuthentication();
             app.UseAuthorization();
-
+         
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                  name: "Admin",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
