32a05198e47b1b8465e96cf9fe8fca08546da5ce
Ashley Laswell <32974016+ashleylaswell@users.noreply.github.com>
2022-01-10 17:56:19 -0500

    add guid's for admin user and role

---
+++ AWBugTracker/Data/Migrations/20220110214841_AddAdminAccount.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AWBugTracker.Data.Migrations
+{
+    public partial class AddAdminAccount : Migration
+    {
+        const string ADMIN_USER_GUID = "0af5a492-6769-44a1-a2a0-9d6f7c29ca16";
+        const string ADMIN_ROLE_GUID = "cc7ff2f7-91ec-4d28-991f-4aae5f4e1af7";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
