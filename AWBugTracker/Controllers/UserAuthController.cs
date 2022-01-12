8548eac47d32d05e1bccae940cefe4443914757d
Ashley Laswell <32974016+ashleylaswell@users.noreply.github.com>
2022-01-12 14:16:13 -0500

    add controller for user authentication

---
+++ AWBugTracker/Controllers/UserAuthController.cs
@@ -0,0 +1,67 @@
+﻿using AWBugTracker.Data;
+using AWBugTracker.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWBugTracker.Controllers
+{
+    public class UserAuthController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public UserAuthController(ApplicationDbContext context,
+                                    UserManager<ApplicationUser> userManager,
+                                    SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            loginModel.LoginInvalid = "true";
+
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginModel.Email,
+                                                                        loginModel.Password,
+                                                                        loginModel.RememberMe,
+                                                                        lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    loginModel.LoginInvalid = "";
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt");
+                }
+            }
+            return PartialView("_UserLoginPartial", loginModel);
+        }
+        
+        public async Task<IActionResult> Logout(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
