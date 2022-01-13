e7f543b6ee6cbcffed6fd289f871e309b835030e
Ashley Laswell <32974016+ashleylaswell@users.noreply.github.com>
2022-01-13 14:37:06 -0500

    add registration model

---
+++ AWBugTracker/Models/RegistrationModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWBugTracker.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "User Name")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]{5}(?:-[0-9]{4})?$")]
+        [Display(Name = "ZIP Code")]
+        public string ZipCode { get; set; }
+
+        [Required]
+        [RegularExpression(@"(\s*\(?0\d{4}\)?\s*\d{6}\s*)|(\s*\(?0\d{3}\)?\s*\d{3}\s*\d{4}\s*)")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        public bool AcceptUserAgreement { get; set; }
+        public string RegistrationInvalid { get; set; }
+        public int ProjectId { get; set; }
+
+    }
+}
