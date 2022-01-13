a04686df18c504c72bb0dcf1465525a59e6bb7d2
Ashley Laswell <32974016+ashleylaswell@users.noreply.github.com>
2022-01-13 18:39:34 -0500

    add method to register a user

--- AWBugTracker/Controllers/UserAuthController.cs
+++ AWBugTracker/Controllers/UserAuthController.cs
@@ -3,6 +3,7 @@ using AWBugTracker.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -49,7 +50,10 @@ namespace AWBugTracker.Controllers
             }
             return PartialView("_UserLoginPartial", loginModel);
         }
-        
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Logout(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
@@ -62,6 +66,75 @@ namespace AWBugTracker.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
+
         }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegisterUser(RegistrationModel registrationModel)
+        {
+            registrationModel.RegistrationInvalid = "true";
+
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    UserName = registrationModel.Email,
+                    Email = registrationModel.Email,
+                    PhoneNumber = registrationModel.PhoneNumber,
+                    FirstName = registrationModel.FirstName,
+                    LastName = registrationModel.LastName,
+                    Address1 = registrationModel.Address1,
+                    Address2 = registrationModel.Address2,
+                    ZipCode = registrationModel.ZipCode
+
+                };
+
+                var result = await _userManager.CreateAsync(user, registrationModel.Password);
+
+                if (result.Succeeded)
+                {
+                    registrationModel.RegistrationInvalid = "";
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return PartialView("_UserRegistrationPartial", registrationModel);
+                }
+
+                AddErrorsToModelState(result);
+
+            }
+            return PartialView("_UserRegistrationPartial", registrationModel);
+
+        }
+
+        [AllowAnonymous]
+        public async Task<bool> UserNameExists(string userName)
+        {
+            bool userNameExists = await _context.Users.AnyAsync(u => u.UserName.ToUpper() == userName.ToUpper());
+
+            if (userNameExists)
+                return true;
+
+            return false;
+
+        }
+
+        private void AddErrorsToModelState(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+                ModelState.AddModelError(string.Empty, error.Description);
+        }
+
+        //private async Task AddProjectToUser(string userId, int projectId)
+        //{
+        //    UserCategory userCategory = new UserCategory();
+        //    userCategory.ProjectId = projectId;
+        //    userCategory.UserId = userId;
+        //    _context.UserCategory.Add(userCategory);
+        //    await _context.SaveChangesAsync();
+        //}
     }
 }
+
