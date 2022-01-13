4f4a59716938907ab2bb92f23f14e2ab5f30417d
Ashley Laswell <32974016+ashleylaswell@users.noreply.github.com>
2022-01-13 18:40:35 -0500

    fix regular expression for phone number and remove required annotations from address, zip, and phone

--- AWBugTracker/Models/RegistrationModel.cs
+++ AWBugTracker/Models/RegistrationModel.cs
@@ -33,17 +33,15 @@ namespace AWBugTracker.Models
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [Required]
+
         public string Address1 { get; set; }
         public string Address2 { get; set; }
 
-        [Required]
-        [RegularExpression("^[0-9]{5}(?:-[0-9]{4})?$")]
+        [RegularExpression(@"^[0-9]{5}(?:-[0-9]{4})?$")]
         [Display(Name = "ZIP Code")]
         public string ZipCode { get; set; }
 
-        [Required]
-        [RegularExpression(@"(\s*\(?0\d{4}\)?\s*\d{6}\s*)|(\s*\(?0\d{3}\)?\s*\d{3}\s*\d{4}\s*)")]
+        [RegularExpression(@"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}")]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public bool AcceptUserAgreement { get; set; }
