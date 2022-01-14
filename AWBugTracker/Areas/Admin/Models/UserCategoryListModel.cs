27d210c1249ff653fb473ee3e845a2ec419f3073
Ashley Laswell <32974016+ashleylaswell@users.noreply.github.com>
2022-01-14 15:38:33 -0500

    add user category list model

---
+++ AWBugTracker/Areas/Admin/Models/UserCategoryListModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWBugTracker.Areas.Admin.Models
+{
+    public class UserCategoryListModel
+    {
+        public int ProjectId { get; set; }
+        public ICollection<UserModel> Users { get; set; }
+        public ICollection<UserModel> UsersSelected { get; set; }
+    }
+}
